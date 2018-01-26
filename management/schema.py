ab3fc8d14a194d457accc4bb6a5e0c5596e0082a
Felipe Pontes <felipemfpontes@gmail.com>
2018-01-25 23:35:37 -0300

    Add single queries for users

--- management/schema.py
+++ management/schema.py
@@ -26,11 +26,20 @@ class TeacherType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
+    user = graphene.Field(UserType, username=graphene.String())
     users = graphene.List(UserType)
+
+    teacher = graphene.Field(TeacherType, username=graphene.String())
     teachers = graphene.List(TeacherType)
+
+    def resolve_user(self, info, username, **kwargs):
+        return User.objects.get(username=username)
 
     def resolve_users(self, info, **kwargs):
         return User.objects.all()
+
+    def resolve_teacher(self, info, username, **kwargs):
+        return User.objects.exclude(teachersubject=None).get(username=username)
 
     def resolve_teachers(self, info, **kwargs):
         return User.objects.exclude(teachersubject=None)
