b0899a7028537c41fc6a73a189d4f47b04e83007
Francisco Bento <franciscobdsjunior@gmail.com>
2018-02-25 01:09:53 -0300

    :sparkles: Add query to see teacher's questions
     #4

--- questions/queries.py
+++ questions/queries.py
@@ -2,7 +2,7 @@ import graphene
 
 from questions.models import Difficulty, Question
 from questions.types import DifficultyType, QuestionType
-
+from management.decorators import login_required
 
 class DifficultyQuery(graphene.ObjectType):
 
@@ -20,9 +20,16 @@ class QuestionQuery(graphene.ObjectType):
 
     question = graphene.Field(QuestionType, id=graphene.UUID())
     questions = graphene.List(QuestionType)
+    my_questions = graphene.List(QuestionType)
 
     def resolve_question(self, info, id, **kwargs):
         return Question.objects.get(pk=id)
 
     def resolve_questions(self, info, **kwargs):
         return Question.objects.all()
+
+    @login_required
+    def resolve_my_questions(self, info, **kwargs):
+        user = info.context.user
+        questions = Question.objects.filter(created_by=user.id)
+        return questions
