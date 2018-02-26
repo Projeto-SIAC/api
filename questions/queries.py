22947f1c20b29f2b32c52f54ddcdff1447a96bad
Francisco Bento <franciscobdsjunior@gmail.com>
2018-02-26 13:51:42 -0300

    :hammer: Refactor questions query (#12)
    #11

--- questions/queries.py
+++ questions/queries.py
@@ -2,7 +2,7 @@ import graphene
 
 from questions.models import Level, Question
 from questions.types import LevelType, QuestionType
-from management.decorators import login_required
+from management.decorators import teacher_required
 
 class LevelQuery(graphene.ObjectType):
 
@@ -20,16 +20,12 @@ class QuestionQuery(graphene.ObjectType):
 
     question = graphene.Field(QuestionType, id=graphene.UUID())
     questions = graphene.List(QuestionType)
-    my_questions = graphene.List(QuestionType)
 
     def resolve_question(self, info, id, **kwargs):
         return Question.objects.get(pk=id)
 
+    @teacher_required
     def resolve_questions(self, info, **kwargs):
-        return Question.objects.all()
-
-    @login_required
-    def resolve_my_questions(self, info, **kwargs):
         user = info.context.user
         questions = Question.objects.filter(created_by=user.id)
         return questions
